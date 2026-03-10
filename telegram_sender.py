41c64fb525b32e39f06b565bf4816256787dee31
Bandi Manideep <parzifal297253@gmail.com>
2026-03-10 09:10:06 +0530

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,12 +1,19 @@
-import requests
 import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_message(text):
 
-    url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_BOT_TOKEN')}/sendMessage"
+    # Telegram limit ≈ 4096
+    if len(text) > 3900:
+        text = text[:3900] + "\n\n... (truncated)"
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     payload = {
-        "chat_id": os.getenv("TELEGRAM_CHAT_ID"),
+        "chat_id": CHAT_ID,
         "text": text
     }
 
