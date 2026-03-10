ae54f3059f8e5c2cc87ada984b0489ede9592d57
Bandi Manideep <parzifal297253@gmail.com>
2026-03-10 12:02:41 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ import run_once
 scheduler = BlockingScheduler()
 
 # Run every day at 11:38 AM
-scheduler.add_job(run_once.main, 'interval', minutes=2)
+scheduler.add_job(run_once.main, 'interval', minutes=5)
 
 print("AI OS Scheduler Started...")
 
